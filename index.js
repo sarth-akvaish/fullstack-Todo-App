f01e04255cafd14d5911c7ea92da5c11b237a6fd
sarth-akvaish <108884904+sarth-akvaish@users.noreply.github.com>
2023-06-02 20:13:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require('./routes/TodoRoutes')
+require("dotenv").config()
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() =>  console.log(`Connected to MONGODB`))
+.catch ((e) => console.log(e));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`Listening at Port ${PORT}`);
+})
